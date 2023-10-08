0731f190d8b73c06fe6ac1a7e6ce1a9f5afb3fc1
studymachiney <1771134505@qq.com>
2023-10-08 09:51:07 +0800

    fix: 修改tabnine的快捷键

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -4,7 +4,7 @@ return { {
     config = function()
       require("tabnine").setup({
         disable_auto_comment = true,
-        accept_keymap = "<C-n>",
+        accept_keymap = "<C-a>",
         dismiss_keymap = "<C-]>",
         debounce_ms = 300,
         suggestion_color = { gui = "#808080", cterm = 244 },
